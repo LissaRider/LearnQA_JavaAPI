d2f59b68456dc71039ff1f3e2d33eec474d70be6
Vasilisa Smirnova <lissarider@gmail.com>
2022-09-14 20:19:16 +0300

    Реализован тест testRestAssuredJsonParsing, в котором парсится ответ в формате json Lesson: 05. Парсинг JSON Session: II. Создание простых запросов к API

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -60,5 +61,28 @@ public class HelloWorldTest {
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    /**
+     * II. Создание простых запросов к API
+     * <p>
+     * 05. Парсинг JSON
+     */
+    @Test
+    public void testRestAssuredJsonParsing() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String name = response.get("answer2");
+        if (name == null) {
+            System.out.println("The key 'answer2' is absent");
+        } else {
+            System.out.println(name);
+        }
     }
 }
