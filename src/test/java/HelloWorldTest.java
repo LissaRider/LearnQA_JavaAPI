5a748f484a988e3b01261577981a5e0a68114ed7
Vasilisa Smirnova <lissarider@gmail.com>
2022-09-14 19:46:21 +0300

    Реализован тест, который отправляет GET-запрос по адресу: https://playground.learnqa.ru/api/get_text и выводит содержимое текста в ответе на запрос. Homework: Ex4: GET-запрос Session: I. Настройка окружения и hello world.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,15 +4,40 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
+    /**
+     * I.Настройка окружения и hello world. 09.
+     * <p>
+     * Создание проекта и Hello worldГиперссылка
+     */
     @Test
     public void testHelloWorld() {
-//        I.Настройка окружения и hello world. 09. Создание проекта и Hello worldГиперссылка
-//        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
 
-//        I. Настройка окружения и hello world. 10. Первый API тестГиперссылка
+    /**
+     * I. Настройка окружения и hello world.
+     * <p>
+     * 10. Первый API тестГиперссылка
+     */
+    @Test
+    public void testHelloRequest() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    /**
+     * I. Настройка окружения и hello world.
+     * <p>
+     * Ex4: GET-запрос
+     */
+    @Test
+    public void testGetTextRequest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+//        response.prettyPrint();
+        System.out.println(response.asString());
     }
 }
