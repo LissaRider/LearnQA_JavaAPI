ed946f79817b667c72a38fe4993e8e1e0a9a2050
Vasilisa Smirnova <lissarider@gmail.com>
2022-09-14 21:00:34 +0300

    Реализованы тесты, выводящие заголовки запросов и ответов Lesson: 08. Заголовки Session: II. Создание простых запросов к API

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -181,5 +182,46 @@ public class HelloWorldTest {
                 .andReturn();
         int statusCode = response.getStatusCode();
         System.out.println(statusCode);
+    }
+
+    /**
+     * II. Создание простых запросов к API
+     * <p>
+     * 08. Заголовки
+     */
+    @Test
+    public void testGet303RedirectWithHeadersRequest() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
+        response.prettyPrint();
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+
+    /**
+     * II. Создание простых запросов к API
+     * <p>
+     * 08. Заголовки
+     */
+    @Test
+    public void testShowAllHeadersRequest() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("MyHeader1", "myValue1");
+        headers.put("MyHeader2", "myValue2");
+
+        Response response = RestAssured
+                .given()
+                .headers(headers)
+                .when()
+                .get("https://playground.learnqa.ru/api/show_all_headers")
+                .andReturn();
+        response.prettyPrint();
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
     }
 }
