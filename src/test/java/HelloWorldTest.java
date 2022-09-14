904802f25310e28fadd1e45572660d7401b4d171
Vasilisa Smirnova <lissarider@gmail.com>
2022-09-14 18:48:53 +0300

    Реализован первый тест API Lesson: 10. Первый API тест Session: I. Настройка окружения и hello world.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello world");
+    public void testHelloWorld() {
+//        I.Настройка окружения и hello world. 09. Создание проекта и Hello worldГиперссылка
+//        System.out.println("Hello world");
+
+//        I. Настройка окружения и hello world. 10. Первый API тестГиперссылка
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
