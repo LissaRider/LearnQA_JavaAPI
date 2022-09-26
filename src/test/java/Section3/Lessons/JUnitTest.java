ac6d918cf3a42b59ee01dc1cd546504f45a5cd1e
Vasilisa Smirnova <lissarider@gmail.com>
2022-09-26 14:10:25 +0300

    Реализованы простые тесты c проверками, использующими библиотеку JUnit. Homework: 01. JUnit и простые тесты Session: III. Написание тестов

---
+++ src/test/java/Section3/Lessons/JUnitTest.java
@@ -0,0 +1,54 @@
+package Section3.Lessons;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * III. Написание тестов
+ * <p>
+ * 01. JUnit и простые тесты
+ */
+public class JUnitTest {
+
+//    @Test
+//    public void testPassedMapRequest() {
+//
+//        Response response = RestAssured
+//                .get("https://playground.learnqa.ru/api/map")
+//                .andReturn();
+////        assertTrue(response.getStatusCode() == 200, "Unexpected status code");
+//        assertEquals(200, response.getStatusCode(), "Unexpected status code");
+//    }
+//
+//    @Test
+//    public void testFailedMapRequest() {
+//
+//        Response response = RestAssured
+//                .get("https://playground.learnqa.ru/api/map2")
+//                .andReturn();
+//        assertEquals(200, response.getStatusCode(), "Unexpected status code");
+//    }
+
+    @Test
+    public void testFor200MapRequest() {
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        assertEquals(200, response.getStatusCode(), "Unexpected status code");
+    }
+
+    @Test
+    public void testFor404MapRequest() {
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2")
+                .andReturn();
+        assertEquals(404, response.getStatusCode(), "Unexpected status code");
+    }
+
+}
